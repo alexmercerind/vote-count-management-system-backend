c106b0fb465c86d0a8f7e9718eeb54f598410efc
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-04-26 00:57:53 +0530

    feat: Candidate entity

---
+++ src/main/java/com/alexmercerind/votecountmanagementsystem/entity/Candidate.java
@@ -0,0 +1,35 @@
+
+package com.alexmercerind.votecountmanagementsystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "candidate")
+public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "candidate_id", nullable = false, columnDefinition = "serial")
+    private int candidateId;
+
+    @Column(name = "candidate_name", nullable = false)
+    private String candidateName;
+
+    @Column(name = "candidate_party", nullable = false)
+    private String candidateParty;
+
+    @Column(name = "candidate_address", nullable = false)
+    private String candidateAddress;
+}
