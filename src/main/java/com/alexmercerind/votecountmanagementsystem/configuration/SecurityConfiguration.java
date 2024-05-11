a77f8e15f27bece35ebb0510d05f767a087c8903
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-05-11 11:15:17 +0530

    fix: compile

--- src/main/java/com/alexmercerind/votecountmanagementsystem/configuration/SecurityConfiguration.java
+++ src/main/java/com/alexmercerind/votecountmanagementsystem/configuration/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
